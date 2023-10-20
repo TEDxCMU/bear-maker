6b3e9a8797a1bf5b83afdc602b5ca2ce588e3e4e
SirBillyBob <aaron.husun@gmail.com>
2023-10-20 01:09:26 -0400

    Download and Share Buttons added
    Download and share buttons added with minimal functionality, need to create and add graphics + more share buttons

--- bear-maker/src/app/charMaker.js
+++ bear-maker/src/app/charMaker.js
@@ -3,6 +3,7 @@ import Selector from "./components/selector"
 import Display from "./components/display"
 import constantObject from "./components/constants"
 import { useState } from "react";
+import { FacebookShareButton, TwitterShareButton } from 'react-share';
 
 export default function CharMaker() {
     const [img, setImg] = useState("/placeholder.png");
@@ -10,10 +11,26 @@ export default function CharMaker() {
         console.log(e.currentTarget)
        setImg(e.currentTarget.getAttribute("data"))
     }; 
+    const downloadImage = () => {
+        const link = document.createElement('a');
+        link.href = img;
+        link.download = 'image.jpg';
+        link.click();
+      };
    
     return <>
         <Display imgUrl={img}></Display>
         <Selector images={constantObject.faces} updateFunction={updateImage}></Selector>
         <div>hi</div> 
+        <button onClick={downloadImage}>Download</button>
+        <FacebookShareButton url={img}>
+            Share on Facebook 
+        </FacebookShareButton> 
+            
+        <TwitterShareButton url={img}>
+            Share on Twitter
+        </TwitterShareButton>
     </>
+    
 }
+
