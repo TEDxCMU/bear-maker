17b897c913e580845b164c372ecd11bb78824ce2
Ethan Huang <yehuang2020@gmail.com>
2023-11-04 15:10:42 -0400

    fixed size of selction images

--- bear-maker/src/app/components/selector.js
+++ bear-maker/src/app/components/selector.js
@@ -5,7 +5,7 @@ export default function Selector(props) {
 
     const imgs = imageList.map((img) => (
         <div className="optionImage" key={img.id} onClick={props.updateFunction} data={img.url}>
-          <img src={img.thumb != "" ? img.thumb : img.url} alt={img.alt} width="100px" height="100px"/>
+          <img src={img.thumb != "" ? img.thumb : img.url} alt={img.alt} width="140px" height="140px"/>
          </div>  
     ))
 
