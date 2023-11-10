5f91d317fa4abcf80f4b49704b9645b2737077da
Emily Xie <30580023+emiilyxie@users.noreply.github.com>
2023-11-10 18:51:03 -0500

    add styling changes for selecting things

--- bear-maker/src/app/components/constants.js
+++ bear-maker/src/app/components/constants.js
@@ -2,6 +2,12 @@ const constantObject = {
     "head": [
         {"id":0, "thumb": "/no-image.png", "url": "/no-image.png", "alt":""},
         {"id":1, "thumb": "/thumbnails/winter-hat.jpeg", "url":"/head/winter-hat.png", "alt":"Sparkle grin face"},
+        {"id":2, "thumb": "/no-image.png", "url": "/no-image.png", "alt":""},
+        {"id":3, "thumb": "/no-image.png", "url": "/no-image.png", "alt":""},
+        {"id":4, "thumb": "/no-image.png", "url": "/no-image.png", "alt":""},
+        {"id":5, "thumb": "/no-image.png", "url": "/no-image.png", "alt":""},
+        {"id":6, "thumb": "/no-image.png", "url": "/no-image.png", "alt":""},
+        {"id":7, "thumb": "/no-image.png", "url": "/no-image.png", "alt":""},
     ],
     "hand1": [
         {"id":0, "thumb": "/no-image.png", "url": "/no-image.png", "alt":""},
