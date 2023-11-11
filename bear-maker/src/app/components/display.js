2b6ebb8c6d66699292f128b9e2d0a89d1939c8b3
Emily Xie <30580023+emiilyxie@users.noreply.github.com>
2023-11-10 19:09:48 -0500

    styling changes to bear image

--- bear-maker/src/app/components/display.js
+++ bear-maker/src/app/components/display.js
@@ -5,10 +5,10 @@ export default function Display(props) {
     const width = "750"
     const height = "750"
     return <>
-        <Image className="bear_image" src="/base.png" width={width} height={height} alt="character profile image"></Image>
-        {imgs.head && imgs.head != noImageUrl ? <Image className="bear_image" src={imgs.head} width={width} height={height} alt="character profile image"></Image>:<></>}
-        {imgs.body && imgs.body != noImageUrl ? <Image className="bear_image" src={imgs.body} width={width} height={height} alt="character profile image"></Image>:<></>}
-        {imgs.hand1 && imgs.hand1 != noImageUrl ? <Image className="bear_image" src={imgs.hand1} width={width} height={height} alt="character profile image"></Image>:<></>}
-        {imgs.hand2 && imgs.hand2 != noImageUrl ? <Image className="bear_image" src={imgs.hand2} width={width} height={height} alt="character profile image"></Image>:<></>}
+        <Image className="bear_image" src="/base.png" fill style={{objectFit:"contain"}} alt="character profile image"></Image>
+        {imgs.head && imgs.head != noImageUrl ? <Image className="bear_image" src={imgs.head} fill style={{objectFit:"contain"}} alt="character profile image"></Image>:<></>}
+        {imgs.body && imgs.body != noImageUrl ? <Image className="bear_image" src={imgs.body} fill style={{objectFit:"contain"}} alt="character profile image"></Image>:<></>}
+        {imgs.hand1 && imgs.hand1 != noImageUrl ? <Image className="bear_image" src={imgs.hand1} fill style={{objectFit:"contain"}} alt="character profile image"></Image>:<></>}
+        {imgs.hand2 && imgs.hand2 != noImageUrl ? <Image className="bear_image" src={imgs.hand2} fill style={{objectFit:"contain"}} alt="character profile image"></Image>:<></>}
     </>
 }
